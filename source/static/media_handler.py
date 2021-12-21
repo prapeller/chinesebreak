e3e2c84a623df56579ed222472556bb14ea51a00
prapeller <prapeller@mail.ru>
2021-12-21 10:56:11 +0300

    corrected media handlers for ubuntu

--- source/static/media_handler.py
+++ source/static/media_handler.py
@@ -9,7 +9,7 @@ def add_to_task_video(task, file):
     filename = file.filename  # asdfagsd.mp4
     ext_type = filename.split('.')[-1]  # mp4
     file_type = 'video'
-    static_path = 'static\\video'
+    static_path = 'static/video'
     filepath = os.path.join(current_app.root_path, static_path)  # .../source/static/video/
     storage_filename = f'{str(type(task)).split(".")[-1][:-2].lower()}_{task.id}_{file_type}.{ext_type}'  # task_2_video.png
     file.save(os.path.join(filepath, storage_filename))
@@ -30,7 +30,7 @@ def add_to_topic_image(topic, file):
     filename = file.filename  # asdfagsd.jpg
     ext_type = filename.split('.')[-1]  # jpg\
     file_type = 'image'
-    static_path = 'static\image'
+    static_path = 'static/image'
     filepath = os.path.join(current_app.root_path,
                             static_path)  # .../source/static/image/  || .../source/static/audio/ || .../source/static/video/
     storage_filename = f'{str(type(topic)).split(".")[-1][:-2].lower()}_{topic.id}_{file_type}.{ext_type}'  # task_2_image_0.png / task_2_image_1.png
@@ -53,7 +53,7 @@ def add_to_word_image(word, file):
     filename = file.filename  # asdfagsd.jpg
     ext_type = filename.split('.')[-1]  # jpg\
     file_type = 'image'
-    static_path = 'static\image'
+    static_path = 'static/image'
     filepath = os.path.join(current_app.root_path,
                             static_path)  # .../source/static/image/  || .../source/static/audio/ || .../source/static/video/
     storage_filename = f'{str(type(word)).split(".")[-1][:-2].lower()}_{word.id}_{file_type}.{ext_type}'  # task_2_image_0.png / task_2_image_1.png
@@ -77,7 +77,7 @@ def add_to_task_image(task, file):
     filename = file.filename  # asdfagsd.jpg
     ext_type = filename.split('.')[-1]  # jpg\
     file_type = 'image'
-    static_path = 'static\image'
+    static_path = 'static/image'
     filepath = os.path.join(current_app.root_path,
                             static_path)  # .../source/static/image/  || .../source/static/audio/ || .../source/static/video/
     count = len(task.media['sent_images_id'])
@@ -97,7 +97,7 @@ def add_to_word_audio(word, file):
     ext_type = filename.split('.')[-1]  # mp3\
     assert ext_type in ['mp3', 'wav']
     file_type = 'audio'
-    static_path = 'static\\audio'
+    static_path = 'static/audio'
     filepath = os.path.join(current_app.root_path, static_path)  # .../source/static/audio/
     storage_filename = f'{str(type(word)).split(".")[-1][:-2].lower()}_{word.id}_{file_type}.{ext_type}'  # task_2_image_0.png / task_2_image_1.png
     file.save(os.path.join(filepath, storage_filename))
@@ -119,7 +119,7 @@ def add_to_task_sent_A_audio(task, file):
     ext_type = filename.split('.')[-1]  # mp3\
     assert ext_type in ['mp3']
     file_type = 'audio'
-    static_path = 'static\\audio'
+    static_path = 'static/audio'
     filepath = os.path.join(current_app.root_path, static_path)  # .../source/static/audio/
     storage_filename = f'task_{task.id}_sent_A_audio.{ext_type}'  # task_2_sent_A_audio.mp3
     file.save(os.path.join(filepath, storage_filename))
@@ -141,7 +141,7 @@ def add_to_task_sent_B_audio(task, file):
     ext_type = filename.split('.')[-1]  # mp3\
     assert ext_type in ['mp3']
     file_type = 'audio'
-    static_path = 'static\\audio'
+    static_path = 'static/audio'
     filepath = os.path.join(current_app.root_path, static_path)  # .../source/static/audio/
     storage_filename = f'task_{task.id}_sent_B_audio.{ext_type}'  # task_2_sent_A_audio.mp3
     file.save(os.path.join(filepath, storage_filename))
